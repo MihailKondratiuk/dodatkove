b291e28fbeef0c058840da25253b4932be5a5796
Mihail <41229374+MihailKondratiuk@users.noreply.github.com>
2021-12-24 10:44:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('foto1').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto1').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('foto2').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto2').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('foto3').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto3').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('foto4').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto4').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('foto5').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto5').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('foto6').onclick = function(){
+  document.getElementById('modalka').src = document.getElementById('foto6').src;
+  document.getElementById('modal-view').style = "display:block";
+}
+document.getElementById('modal-view').onclick = function(){
+  document.getElementById('modal-view').style = "display:none";
+}
